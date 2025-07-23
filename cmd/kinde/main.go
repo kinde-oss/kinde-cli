9823dc6c7ae4e52e225728eb7d87e20006d40a34
evgenyk <evgeny@kinde.com>
2025-07-23 23:20:00 +1000

    improved timeout

--- cmd/kinde/main.go
+++ cmd/kinde/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"context"
+	"time"
 
 	"github.com/kinde-oss/kinde-cli/pkg/cmd"
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 10)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 	cmd.Execute(ctx)
 }
