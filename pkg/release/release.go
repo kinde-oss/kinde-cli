c29e15d341db43b1592749056a1691140ff4145b
evgenyk <evgeny@kinde.com>
2025-08-20 08:59:50 +1000

    feat: add spinner for update check and improve version command output

--- pkg/release/release.go
+++ pkg/release/release.go
@@ -3,8 +3,11 @@ package release
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"strings"
+	"time"
 
+	"github.com/briandowns/spinner"
 	"github.com/google/go-github/v28/github"
 )
 
@@ -16,15 +19,20 @@ var (
 
 func IsNeedingUpdate() {
 	client := github.NewClient(nil)
+	fmt.Printf("Checking for updates... \n")
+	s := spinner.New(spinner.CharSets[rand.Intn(90)], 100*time.Millisecond)
+	s.Start()
+
 	rep, _, err := client.Repositories.GetLatestRelease(context.Background(), "kinde-oss", "kinde-cli")
+
+	s.Stop()
 
 	if err != nil {
 		return
 	}
 
 	latest := *rep.TagName
-
 	if strings.TrimPrefix(Version, "v") != strings.TrimPrefix(latest, "v") {
-		fmt.Printf("An update is available: %v", latest)
+		fmt.Printf("An update is available: %v\n", latest)
 	}
 }
